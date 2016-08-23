f3514c92313ae232068e42b9f915515fc79332e7
Gustavo Buarque Costa Cardoso <gustavobuarque@gmail.com>
2016-08-23 11:49:23 -0300

    Adicionado função de refresh

--- mural2.php
+++ mural2.php
@@ -44,6 +44,30 @@ jQuery(document).ready(function(){
 
   } // end loadJSON
 
+  /*Função para dar refresh na página*/
+  function refreshAt(hours, minutes, seconds, day) {
+    var now = new Date();
+    var then = new Date();
+    var dayUTC = new Date();
+
+    if(dayUTC.getUTCDay() == day) {
+
+        if(now.getUTCHours() > hours ||
+       (now.getUTCHours() == hours && now.getUTCMinutes() > minutes) ||
+        now.getUTCHours() == hours && now.getUTCMinutes() == minutes && now.getUTCSeconds() >= seconds) {
+        then.setUTCDate(now.getUTCDate() + 1);
+        }
+
+    then.setUTCHours(hours);
+    then.setUTCMinutes(minutes);
+    then.setUTCSeconds(seconds);
+
+
+    var timeout = (then.getTime() - now.getTime());
+      setTimeout(function() { window.location.reload(true); }, timeout);
+    }
+  }
+
   function renderList(result){
 
     $("#ticker").empty();
@@ -122,6 +146,7 @@ jQuery(document).ready(function(){
     } 
   };
 
+  refreshAt(12,00,0,1); //Will refresh the page at 8:00pm on Monday UTC or 3:00pm EST
   loadJSON();
 
 });
